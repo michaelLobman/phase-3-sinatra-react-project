a27f5235b0ccca94d76c7361cfdb34ec6e9a9f3a
Michael Lobman <michael.lobman@gmail.com>
2022-01-26 12:39:08 -0500

    Initialize get '/bottles/'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,20 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    macallan = Distillery.find(1)
+    bottle_arr = macallan.bottles.map{|bottle| bottle.name }
+    bottle_arr.to_json
+  end
+
+  get "/bottles" do
+    Bottle.all.order(:aged_in_years).to_json(only: [:name, :aged_in_years])    
+  end
+
+  get "/distilleries/:id/bottles" do 
+    distillery = Distillery.find(params[:id])
+    distillery.bottles.order(:aged_in_years).to_json(only: [:name, :aged_in_years])
   end
 
 end
+
+
--- app/models/Distillery.rb
+++ app/models/Distillery.rb
@@ -1,4 +1,12 @@
 class Distillery < ActiveRecord::Base
     belongs_to :region
     has_many :bottles
+
+    def distillery_bottles
+        self.bottles.map{|bottle| bottle.name}
+    end
+
+    # macallan = Distillery.find(1)
+    # bottle_arr = macallan.bottles.map{|bottle| bottle.name }
+    # bottle_arr.to_json
 end
